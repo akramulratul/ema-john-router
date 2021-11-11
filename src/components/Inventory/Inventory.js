b9c485b4098714d14a5d7a7909f9a8437d232588
akramulratul <68425589+akramulratul@users.noreply.github.com>
2021-11-12 02:39:58 +0600

    inventory js

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Inventory = () => {
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores iste
+        quam odit praesentium consequuntur!
+      </h1>
+    </div>
+  );
+};
+
+export default Inventory;
